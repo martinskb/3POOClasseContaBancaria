9289cece22bf87d9da5761fde512dc63e17eef18
kaio <kaio22barra@gmail.com>
2025-09-01 16:59:09 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/*
+3. Classe ContaBancaria (Encapsulamento + Métodos Públicos)
+• Crie a classe ContaBancaria com atributos privados:
+        ◦ numeroConta (int)
+◦ saldo (double)
+• Crie um construtor que inicializa o número da conta e saldo inicial.
+        • Implemente métodos públicos:
+        ◦ depositar(double valor) → soma ao saldo
+◦ sacar(double valor) → subtrai do saldo se houver saldo suficiente
+◦ getSaldo() → retorna o saldo atual
+• No main, crie uma conta e faça depósitos e saques.
+ */
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        ContaBancaria conta1 = new ContaBancaria(101, 1000);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.print("Digite o valor do depósito: ");
+        double valor = sc.nextDouble();
+        conta1.deposito(valor);
+
+        System.out.print("Digite o valor do saque: ");
+        double valor2 = sc.nextDouble();
+        conta1.sacar(valor2);
+
+        System.out.println("Saldo atual: " + conta1.getSaldo());
     }
 }
