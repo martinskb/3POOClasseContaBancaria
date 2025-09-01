6de5aacc4fee31fbdfc08b504c3a0485654c9b04
kaio <kaio22barra@gmail.com>
2025-09-01 16:58:50 -0300

    Update ContaBancaria.java

--- src/ContaBancaria.java
+++ src/ContaBancaria.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
-
 public class ContaBancaria {
+    private int numeroConta;
+    private double saldo;
+
+    public ContaBancaria(int numeroConta) {
+        this.numeroConta = numeroConta;
+        this.saldo = 0;
+    }
+
+    public ContaBancaria(int numeroConta, double saldo) {
+        this.numeroConta = numeroConta;
+        this.saldo = saldo;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void deposito(double valor) {
+        saldo += valor;
+    }
+    public void sacar(double valor){
+        if(saldo >= valor){
+            saldo -= valor;
+        } else {
+            System.out.println("Saldo insuficiente");
+        }
+    }
 }
